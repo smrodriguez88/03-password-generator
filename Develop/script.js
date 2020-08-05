32a2ce587b3c5406eb9f148a55da566c69425217
Steve Rodriguez <smrodriguez88@gmail.com>
2020-08-04 23:12:07 -0500

    Set variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var letterArray = ("qwertyuiopasdfghjklzxcvbnm")
+var letterArrayLower = letterArray.toLowerCase().split("");
+var letterArrayUpper = letterArray.toUpperCase().split("");
+var numArray = ("1234567890").split("");
+var spclChrArray = ("~!@#$%^&*()_+`-=[]{};':,./<>?\\|").split("");
+
+// User Input
+
+// Generate Password
 
 // Write password to the #password input
 function writePassword() {
